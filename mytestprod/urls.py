dfe3eb0d92bd587b4443cba76df83f7d1ed8bb9d
Temirlan <temirzhanuly01@mail.ru>
2024-03-12 20:14:33 +0500

    added templates

--- mytestprod/urls.py
+++ mytestprod/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('firstpage/', views.runsrv, name='hello')
+    path('firstpage/', views.runsrv, name='hello'),
+    path('todos/', views.todos, name='todos')
 ]
--- mytestprod/views.py
+++ mytestprod/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoItem
 # Create your views here.
 
 def runsrv(request):
-    return HttpResponse("firstPage")
+    return render(request, 'home.html')
+
+
+def todos(request):
+    items = ToDoItem.objects.all()
+    return render(request, 'todos.html', {'todos': items})
